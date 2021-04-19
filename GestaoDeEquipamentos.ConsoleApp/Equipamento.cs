7cd5bb8375a47eb4cadfaa401c4f53aecb3a9591
SirLucasWolff <lucaswolff2014@hotmail.com>
2021-04-19 13:40:02 -0300

    Add project files.

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeEquipamentos.ConsoleApp
+{
+    public class Equipamento
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string  fabricante { get; set; }
+        public string numero { get; set; }
+        public double valor { get; set; }
+        public DateTime dataFabricacao { get; set; }
+
+        public Equipamento registrar()
+        {
+            Equipamento equipamento = new Equipamento();
+            bool NomeInvalido = false;
+            do
+            {
+                Console.WriteLine("Digite o nome do equipamento");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("No mínimo 6 digitos");
+
+                Console.ResetColor();
+
+                equipamento.nome = Console.ReadLine();
+                if (equipamento.nome.Length <= 6)
+                {
+                    NomeInvalido = true;
+                    Console.WriteLine("Quantidade de caracteres inválidas");
+                }
+
+                continue;
+
+            } while (NomeInvalido);
+
+            Console.WriteLine("Digite o preço do equipamento");
+
+            equipamento.valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o Número de série");
+
+            equipamento.numero = Console.ReadLine();
+
+            Console.WriteLine("Digite a data de fabricação");
+
+            equipamento.dataFabricacao = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da fabricante");
+
+            equipamento.fabricante = Console.ReadLine();
+            
+            Random rnd = new Random();
+            equipamento.id = rnd.Next();
+            
+            return equipamento;
+        }
+
+        public void mostrar(List<Equipamento> equipamentos)
+        {
+            Console.WriteLine("----- Lista de equipamentos -----");
+            foreach (var item in equipamentos)
+            {
+                Console.WriteLine($"ID: {item.id}");
+                Console.WriteLine($"Fabricante: {item.fabricante}");
+                Console.WriteLine($"Nome: {item.nome}");
+                Console.WriteLine($"Numero série: {item.numero}");
+                Console.WriteLine($"Valor: {item.valor}");
+                Console.WriteLine($"Data Fabricação: {item.dataFabricacao}");
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        public void editar(Equipamento equipamento)
+        {
+            bool sair = true;
+            do
+            {
+                Console.WriteLine($"1 - Fabricante: {equipamento.fabricante}");
+                Console.WriteLine($"2 - Nome: {equipamento.nome}");
+                Console.WriteLine($"3 - Numero série: {equipamento.numero}");
+                Console.WriteLine($"4 - Valor: {equipamento.valor}");
+                Console.WriteLine($"5 - Data Fabricação: {equipamento.dataFabricacao}");
+                Console.WriteLine($"Digite o valor correspondente a propriedade que deseja editar ou 6 para sair:");
+                var opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o novo fabricante: ");
+                        equipamento.fabricante = Console.ReadLine();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Digite o novo nome: ");
+                        equipamento.nome = Console.ReadLine();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Digite o novo número de série: ");
+                        equipamento.numero = Console.ReadLine();
+                        break;
+
+                    
+                        
+
+                    default:
+                        sair = false;
+                        break;
+                }
+
+            } while (sair);
+        }
+    }
+
+
+}
---
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeEquipamentos.ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Equipamento> equipamentos = new List<Equipamento>();
+            List<Chamado> chamados = new List<Chamado>();
+            Chamado chamado = new Chamado();
+                
+            Equipamento equipamento = new Equipamento();
+            string menu;
+
+            bool sair = true;
+            do
+            {
+                menu = OpcaoMenu();
+
+                if (menu == "1")
+                {
+                    var equipTemp = equipamento.registrar();
+                    equipamentos.Add(equipTemp);
+                    continue;
+                }
+                if (menu == "2")
+                {
+                    equipamento.mostrar(equipamentos);
+                    continue;
+                }
+                if (menu == "3")
+                {
+                    Console.WriteLine("Excluindo equipamentos....\n");
+
+                    equipamento.mostrar(equipamentos);
+
+                    Console.WriteLine("Digite o número do equipamento que deseja excluir");
+
+                    int numeroSelecionado = Convert.ToInt32(Console.ReadLine());
+
+                    var equipamentoTemp = equipamentos;
+
+                    for (int i = 0; i < equipamentos.Count; i++)
+                    {
+                        if (equipamentos[i].id == numeroSelecionado)
+                        {
+                            equipamentos.RemoveRange(i, 1);
+                        }
+                    }
+
+                    Console.WriteLine("Equipamento excluído com sucesso! \n");
+                    continue;
+                }
+                if (menu == "4")
+                {
+                    if (equipamentos.Count == 0)
+                    {
+                        Console.WriteLine("Nenhum equipamento cadastrado....\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Editando equipamentos....\n");
+
+                        equipamento.mostrar(equipamentos);
+
+                        Console.WriteLine("Digite o número do equipamento que deseja editar");
+
+                        int numeroSelecionado = Convert.ToInt32(Console.ReadLine());
+
+                        foreach (var item in equipamentos)
+                        {
+                            if (item.id == numeroSelecionado)
+                            {
+                                equipamento.editar(item);
+                            }
+                        }
+                    }
+
+                    continue;
+                }
+                if (menu == "5")
+                {
+                    chamados.Add( chamado.registrar());
+                    continue;
+                }
+                if (menu == "6")
+                {
+                    chamado.mostrar(chamados);
+                    continue;
+                }
+
+                if (menu == "7")
+                {
+                    ExcluirChamados(chamados, chamado);
+                }
+
+                if (menu =="8")
+                {
+                    if (chamados.Count == 0)
+                    {
+                        Console.WriteLine("Nenhuma Chamada cadastrada....\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Editando Chamada....\n");
+
+                        chamado.mostrar(chamados);
+
+                        Console.WriteLine("Digite o número da Chamada que deseja editar");
+
+                        int numeroSelecionado = Convert.ToInt32(Console.ReadLine());
+
+                        foreach (var item in chamados)
+                        {
+                            if (item.id == numeroSelecionado)
+                            {
+                                chamado.Editar(item);
+                            }
+                        }
+                    }
+                }
+
+
+                if (menu.Equals("0"))
+                {
+                    sair = false;
+                }
+
+            } while (sair);
+
+
+            //equipamentos.Add(equipamento.registrar());
+
+            //if (menu == "1")
+            //{
+            //    Console.WriteLine("Digite o número do equipamento:");
+            //    int numeroEquipamento = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Digite o título do chamado:");
+            //    string título = Console.ReadLine();
+
+            //    Console.WriteLine("Digite a descrição do chamado:");
+            //    string descricao = Console.ReadLine();
+
+            //    DateTime dataAbertura = DateTime.Now;
+
+            //    Console.WriteLine("Digite 1 para voltar ao menu ou 2 para adicionar mais uma chamada");
+
+            //    Chamada = Console.ReadLine();
+
+            //    if (Chamada == "1")
+            //    {
+            //        return;
+            //    }
+
+            //    if (Chamada == "2")
+            //    {
+
+            //    }
+
+
+            //}
+
+            //}
+            //Console.WriteLine("Digite 1 para voltar ao menu ou 2 para adicionar mais uma chamada");
+
+            //Chamada = Console.ReadLine();
+
+            //if (Chamada == "1")
+            //{
+            //    return;
+            //}
+
+            //if (Chamada == "2")
+            //{
+            //    continue;
+            //}
+
+            //menu = Console.ReadLine();
+
+
+
+        }
+
+        private static void ExcluirChamados(List<Chamado> chamados, Chamado chamado)
+        {
+            Console.WriteLine("Excluindo Chamadas....\n");
+
+            chamado.mostrar(chamados);
+
+            Console.WriteLine("Digite o número da chamada que deseja excluir");
+
+            int numeroSelecionado = Convert.ToInt32(Console.ReadLine());
+
+            var chamadosTemp = chamado;
+
+            for (int i = 0; i < chamados.Count; i++)
+            {
+                if (chamados[i].id == numeroSelecionado)
+                {
+                    chamados.RemoveRange(i, 1);
+                }
+            }
+        }
+
+        private static string OpcaoMenu()
+        {
+            //Menu Interativo
+
+            string menu;
+
+            Console.WriteLine("Registrar Equipamento = 1");
+            Console.WriteLine("Vizualizar Equipamentos  = 2");
+            Console.WriteLine("Excluir Equipamento = 3");
+            Console.WriteLine("Editar Equipamento = 4");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Registrar Chamado= 5");
+            Console.WriteLine("Vizualizar Chamado  = 6");
+            Console.WriteLine("Excluir Chamado = 7");
+            Console.WriteLine("Editar Chamado = 8");
+            Console.WriteLine("Sair = 0");
+
+            
+
+            menu = Console.ReadLine();
+            return menu;
+        }
+    }
+}
